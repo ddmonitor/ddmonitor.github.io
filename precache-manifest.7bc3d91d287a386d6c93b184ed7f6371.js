d2b4b1a8dd900979ee18144c69bcb676747d9f12
电獭 <szlproche@163.com>
2020-01-12 13:21:30 +0800

    init

---
+++ precache-manifest.7bc3d91d287a386d6c93b184ed7f6371.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ebbb28388360101699",
+    "url": "/css/app.731bc5d6.css"
+  },
+  {
+    "revision": "b35dc905b57c8efdc74f",
+    "url": "/css/views.d62c7d68.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "2da4e755619457713537f4491b382c01",
+    "url": "/img/dd.png"
+  },
+  {
+    "revision": "d2ec22e645ccc7912d1994349470de25",
+    "url": "/img/dd.psd"
+  },
+  {
+    "revision": "cd8df1750b0c1c82dde610c13de9083f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94ebbb28388360101699",
+    "url": "/js/app.7d70adbd.js"
+  },
+  {
+    "revision": "439992e4ff4c936052e5",
+    "url": "/js/chunk-vendors.311df311.js"
+  },
+  {
+    "revision": "b35dc905b57c8efdc74f",
+    "url": "/js/views.b23f75c5.js"
+  },
+  {
+    "revision": "35106f4a86290808570a9fcdca0d5211",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ sw.js
@@ -0,0 +1,36 @@
+importScripts("/precache-manifest.7bc3d91d287a386d6c93b184ed7f6371.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+
+workbox.core.setCacheNameDetails({prefix: "dd_monitor"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/\/api\/.+/, new workbox.strategies.NetworkFirst({
+  cacheName: "dd_api"
+}));
+
